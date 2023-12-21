683fe3e0a10b99e46c4fd29017fa199925f3674c
Ryan Luis <RyanCLuis@gmail.com>
2023-12-21 12:21:40 -0800

    Adds Middleware Function

--- server.js
+++ server.js
@@ -4,28 +4,28 @@
 const express = require('express') 
 require('dotenv').config() 
 const path = require('path') 
-
+const middleware = require('./utils/middleware')
 ///////////////////////
 // Import Routers  ////
 ///////////////////////
-
 
 //////////////////////////////////////////////////
 // Create the app object + set up view engine ////
 //////////////////////////////////////////////////
 const app = express() 
 
+// view engine - ejs
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
+
 ///////////////////
 // Middleware  ////
 ///////////////////
+middleware(app)
 
 ///////////////
 // Routes  ////
 ///////////////
-// basic home route
-app.get('/', (req, res) => {
-    res.send('the app is connected')
-})
 
 ////////////////////////
 // Server Listener  ////
@@ -33,7 +33,7 @@ app.get('/', (req, res) => {
 const PORT = process.env.PORT
 
 app.listen(PORT, () => {
-    console.log('Your server is running, better go catch it!')
+    console.log('Your server is running, better go catch it')
 })
 
 // End
---
+++ utils/connection.js
@@ -0,0 +1,26 @@
+///////////////////////////////////
+// Import Dependencies         ////
+///////////////////////////////////
+require('dotenv').config()
+const mongoose = require('mongoose') 
+
+///////////////////////////////////
+// Database Connection         ////
+///////////////////////////////////
+const DATABASE_URL = process.env.DATABASE_URL
+const CONFIG = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}
+
+mongoose.connect(DATABASE_URL, CONFIG)
+
+mongoose.connection
+    .on('open', () => console.log('Connected to Mongoose'))
+    .on('close', () => console.log('Disconnected from Mongoose'))
+    .on('error', (err) => console.log('An error occurred: \n', err))
+
+///////////////////////////////////
+// Export our Connection       ////
+///////////////////////////////////
+module.exports = mongoose
